7c1d47ab6e0ef85258ab1e942ca22de337a87e73
Kit Ropner <ropner.k@etoncollege.org.uk>
2020-11-17 09:41:25 +0000

    .

--- StandardAlgorithms/Sorting.swift
+++ StandardAlgorithms/Sorting.swift
@@ -11,14 +11,23 @@ class Sorting {
     func bubbleSort(data: [Int]) -> [Int]{
         var sortedData = data
         var swaps = 0
-        let length = sortedData.count - 1
-        for i in 0...length{
-            if sortedData[i] > sortedData[i + 1]{
-                sortedData.insert(sortedData[i], at:i+2)
-                sortedData.remove(at: i)
-                swaps += 1
+        if sortedData.count > 0{
+            outerLoop: for i in 0..<sortedData.count-1{
+                swaps = 0
+                if sortedData[i] > sortedData[i + 1]{
+                    sortedData.insert(sortedData[i], at:i+2)
+                    sortedData.remove(at: i)
+                    swaps += 1
+                }
+                if swaps == 0{
+                    break outerLoop
+                }
+               
             }
         }
+        else{
+            return sortedData
+        }
         return sortedData
-    }
+}
 }
