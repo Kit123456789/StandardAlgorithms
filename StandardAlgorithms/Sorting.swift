f68c7286e9561e36f025caf53f5a5b0500830a25
Kit Ropner <ropner.k@etoncollege.org.uk>
2020-11-23 23:04:51 +0000

    .

--- StandardAlgorithms/Sorting.swift
+++ StandardAlgorithms/Sorting.swift
@@ -19,7 +19,7 @@ class Sorting {
                         sortedData.insert(sortedData[i], at:i+2)
                         sortedData.remove(at: i)
                         swaps += 1
-                    if swaps == sortedData.count*2{
+                    if swaps == sortedData.count-1{
                         sorted = true
                     }
                     }
@@ -30,5 +30,57 @@ class Sorting {
             return sortedData
         }
         return sortedData
-}
+    }
+/*    func mergeSort(data: [Int]) -> [Int]{
+        let sortedData = data
+        if sortedData.count > 1{
+            let leftHalf = sortedData[0...sortedData.count/2]
+            let rightHalf = sortedData[sortedData.count/2...sortedData.count-1]
+            if leftHalf.count > 1{
+                var remainder = [Int](rightHalf)
+                return mergeSort(data: Array(leftHalf))
+            }
+            if rightHalf.count > 1{
+                return mergeSort(data: Array(rightHalf))
+            }
+        }
+        else{
+            return sortedData
+        }
+        
+        return sortedData
+    }
+     func quickSort(data:[Int]) -> [Int]{
+        var sortedArray = data
+        var leftArray = [Int]()
+        var rightArray = [Int]()
+        var pivot = sortedArray[0]
+        for numbers in sortedArray{
+         if numbers < pivot{
+           leftArray.append(numbers)
+         if numbers > pivot{
+           rightArray.append(numbers)
+         }
+         }
+        }
+        sortedArray.append(contentsOf: leftArray)
+        sortedArray.append(pivot)
+        sortedArray.append(contentsOf: rightArray)
+        return(quickSort(data: sortedArray))
+
+     }
+     */
+    func insertionSort(data:[Int]) -> [Int]{
+        let unsortedData = data
+        var sortedData = [Int](Array(arrayLiteral: unsortedData[0]))
+        for i in 0..<unsortedData.count-2{
+            if unsortedData[i+1] > sortedData[sortedData.count-1]{
+                sortedData.append(unsortedData[i+1])
+            }
+            else{
+                sortedData.insert(unsortedData[i+1], at: sortedData.count-2)
+            }
+        }
+        return sortedData
+    }
 }
--- StandardAlgorithmsTests/SortingTest.swift
+++ StandardAlgorithmsTests/SortingTest.swift
@@ -20,11 +20,40 @@ class SortingTest: XCTestCase {
     func testBubbleSortWithVariousIntegerArraysReturnsEachOneSorted(){
         //arrange
         let sorting = Sorting()
-        let testData = [(data: [6,4,1,9,2], expected: [1,2,4,6,9]), (data: [1,100,4,3,15], expected: [1,3,4,15,100]), (data: [], expected: [])]
+        let testData = [(data: [6,4,1,2,9], expected: [1,2,4,6,9]), (data: [1,100,4,3,15], expected: [1,3,4,15,100]), (data: [], expected: [])]
         //act
         for test in testData{
             let result = sorting.bubbleSort(data: test.data)
             XCTAssertEqual(result, test.expected)
         }
     }
+    
+/*    func testMergeSortWithIntegerArrayReturnsSortedArray(){
+        //arrange
+        let sorting = Sorting()
+        let expected = [1,3,4,5,6,10,13,32]
+        //act
+        let result = sorting.mergeSort(data: [5,4,3,6,32,10,1,13])
+        //assert
+        XCTAssertEqual(result, expected)
+    }
+     func testQuickSortWithIntegerArrayReturnsSortedArray(){
+             //arrange
+             let sorting = Sorting()
+             let expected = [2,3,5,7,9]
+             //act
+             let result = sorting.quickSort(data: [7,3,9,2,5])
+             //assert
+             XCTAssertEqual(result, expected)
+         }
+ */
+     func testInsertionSortWithIntegerArrayReturnsSortedArray(){
+             //arrange
+             let sorting = Sorting()
+             let expected = [2,4,6,8,10,12]
+             //act
+             let result = sorting.insertionSort(data: [2,8,12,4,10,6])
+             //assert
+             XCTAssertEqual(result, expected)
+         }
 }
